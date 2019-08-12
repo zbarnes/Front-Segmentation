d9ab6124306a3e509d4b2db5ffae4e7b21abdcc9
Zachary Barnes <33704301+zbarnes@users.noreply.github.com>
2019-08-12 15:17:46 -0700

    Add files via upload

---
+++ get_ones_cy.pyx
@@ -0,0 +1,20 @@
+cpdef list get_ones_fast(short [:, :] image):
+    # declare variables
+    cdef int x, y, w, h
+    cdef list ones
+    
+    # initialize variables
+    h = image.shape[0]
+    w = image.shape[1]
+    ones = []
+    
+    # loop over the image
+    for y in range(h): 
+        for x in range(w):
+            # if pixel is 'activated' save it to list
+            if(image[y,x] > 0):
+                ones.append((y,x +1))
+                
+    
+    # return the list of activated pixels image
+    return ones
